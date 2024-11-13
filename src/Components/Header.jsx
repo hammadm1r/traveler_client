b78939b633945e5dc959c8c54f34d576218bdea5
Hammad774 <hammad24197@gmail.com>
2024-11-13 05:00:59 +0500

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import pana from "../assets/Images/Onlineworld-pana1.png";
-const Header = () => {
+const Header = ({ titlePage, desc, img }) => {
   return (
-    <div className="h-28 bg-bgPrimary block md:h-60">
+    <div className="h-28 bg-bgPrimary block md:h-80">
       <div className="h-28 md:block hidden"></div>
-      <div className="h-full  flex justify-between">
-        <div >
-          <p className="text-2xl font-bold text-white md:ml-20 pt-12">Forum Page</p>
+      <div className="h-full flex justify-center md:justify-between ">
+        <div>
+          <p className="text-2xl font-bold text-white md:ml-20 pt-14 ">{titlePage}</p>
           <p className="text-medium font-thin text-white mt-3 md:ml-20 hidden md:block">
-            Share your travel experiences and advices with others.
+            {desc}
           </p>
         </div>
         <div className="mr-20 md:block hidden">
-          <img src={pana} alt="Pana" className="w-60" />
+          <img src={img || pana} alt="Pana" className="w-60" />
         </div>
       </div>
     </div>
   );
 };
+
 
 export default Header;
