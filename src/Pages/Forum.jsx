d10118de5678fd527123c1fbe161bcbe01da1267
Hammad774 <hammad24197@gmail.com>
2024-11-13 05:00:59 +0500

    changes

--- src/Pages/Forum.jsx
+++ src/Pages/Forum.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import pana from '../assets/Images/Onlineworld-pana1.png'
 import Header from '../Components/Header'
 const Forum = () => {
   return (
     <div>
-        <Header />
+        <Header titlePage={'Forum'} desc={'Share your travel experiences and advice with others.'} img={pana}/>
     </div>
   )
 }
