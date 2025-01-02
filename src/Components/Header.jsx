f86351c708c8d2167b86c0c2886ea8b6568f2515
Hammad774 <hammad24197@gmail.com>
2025-01-02 23:30:47 +0500

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import pana from "../assets/Images/Onlineworld-pana1.png";
 const Header = ({ titlePage, desc, img }) => {
   return (
-    <div className="bg-gradient-to-r from-blue-400 to-teal-400 text-white py-4 text-center">
-        <h1 className="text-4xl font-bold">{titlePage}</h1>
-        <p className="mt-2 text-lg">{desc}</p>
-      </div>
-
+    <div className="min-h-24 bg-gradient-to-r from-blue-400 to-teal-400 text-white py-4 text-center">
+    </div>
   );
 };
 
-
 export default Header;
+
