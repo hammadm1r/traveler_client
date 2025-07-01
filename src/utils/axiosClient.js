f9e2bd7425e2f98eb6085ed0b91024d9135d5867
Hammad774 <hammad24197@gmail.com>
2025-07-02 03:20:32 +0500

    Configuration Changes

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { getItem,KEY_ACCESS_TOKEN } from "./LocalStorageManager";
-
-
 const REACT_APP_SERVER_BASE_URL = import.meta.env.REACT_APP_SERVER_BASE_URL;
 export const axiosClient = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL:REACT_APP_SERVER_BASE_URL,
     withCredentials :true,
 })
 
